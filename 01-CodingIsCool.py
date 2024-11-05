2fece371a06095535b632cf667bc050e69de2484
anna <anna.reyes.0210@gmail.com>
2024-11-05 20:53:33 +0400

    Assessment 1

---
+++ 01-CodingIsCool.py
@@ -0,0 +1,9 @@
+## Exercise 1: Coding is Cool - 10 marks
+# Fill in the blanks in the Python code below to output the phrase "Coding is Cool" to the console using variables and string concatenation.
+# Use string concatenation to combine the variables and print the phrase.
+
+## Fill in the blanks below
+word1 = "Coding "
+word2 = "is "
+word3 = "Cool"
+print(word1 + word2 + word3)
